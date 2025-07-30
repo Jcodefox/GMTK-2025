15c52208cc18fe1911deb54cbd352bdc93920828
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-07-30 18:34:15 -0400

    Player script debugging

--- scripts/prefabs/player_v2.gd
+++ scripts/prefabs/player_v2.gd
@@ -9,8 +9,8 @@ extends CharacterBody2D
 @export var air_lateral_accel: float = 0
 
 @export_group("Jumping")
-@export var jump_init_velocity: float = 200
-@export var jump_gravity_multiplier: float = 0.5
+@export var jump_init_velocity: float = -200
+@export var jump_gravity_multiplier: float = 0.6
 @export var coyote_time: float = 0.1
 @export var jump_buffer_time: float = 0.1
 
@@ -20,6 +20,9 @@ var time_since_on_floor: float = INF
 var time_since_jump_attempt: float = INF
 
 func _physics_process(delta: float) -> void:
+	time_since_on_floor += delta
+	time_since_jump_attempt += delta
+	
 	if Input.is_action_just_pressed("move_jump"):
 		time_since_jump_attempt = 0
 	if Input.is_action_pressed("move_jump"):
@@ -37,12 +40,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x += air_lateral_accel * Input.get_axis("move_left", "move_right")
 		velocity *= pow(air_drag, delta)
 	
-	if (
-		(time_since_jump_attempt < jump_buffer_time) and 
-		(time_since_on_floor < coyote_time or jumps_left > 1)
-	):
+	if ((time_since_jump_attempt < jump_buffer_time) and (time_since_on_floor < coyote_time or jumps_left > 0)):
 		jumps_left -= 1
-		velocity.y = -1 * jump_init_velocity
+		velocity.y = jump_init_velocity
 		time_since_jump_attempt = INF
 	
 	move_and_slide()
