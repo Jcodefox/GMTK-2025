d437a97cb478d462287121898fd5000ce74fb9ac
Jcodefox <jcodefox@gmail.com>
2025-07-30 16:43:49 -0400

    Add variable jump height

--- scripts/prefabs/player.gd
+++ scripts/prefabs/player.gd
@@ -13,17 +13,25 @@ extends CharacterBody2D
 @export var jump_force: float = 500
 @export var coyote_time: float = 0.1
 @export var jump_buffer_time: float = 0.1
+@export var max_jump_held_time: float = 1.0
 
-@export_range(0, 100) var double_jumps: int = 0
+@export_range(0, 100) var double_jumps: int = 1
 
+@export_group("Gravity")
+@export var jump_gravity: float = 980
+
+var jump_held_time: float = INF
 var last_jump_attempt: float = INF
 var last_on_floor: float = INF
 @onready var double_jumps_left: int = double_jumps
 
 func _physics_process(delta: float) -> void:
-	var gravity: Vector2 = get_gravity()
+	var gravity_strength: float = ProjectSettings.get_setting("physics/2d/default_gravity") * 2.5
+
+	var current_gravity = jump_gravity if jump_held_time < max_jump_held_time else gravity_strength
+	
 	if not is_on_floor():
-		velocity += gravity * delta
+		velocity.y += current_gravity * delta
 
 	var direction: float = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -47,9 +55,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_jump"):
 		last_jump_attempt = 0
 	
+	if Input.is_action_pressed("move_jump"):
+		jump_held_time += delta
+	else:
+		# Halt jump gravity
+		jump_held_time = INF
+	
 	if last_jump_attempt < coyote_time and (last_on_floor < jump_buffer_time or double_jumps_left > 0):
 		velocity.y = -jump_force
 		double_jumps_left = max(double_jumps_left - 1, 0)
+		jump_held_time = 0
 		# This prevents all double jumps from executing at once
 		last_jump_attempt = INF
 	
