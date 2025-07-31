072dcb1b84caccf5a5614f46e21d30fc545415a1
Jcodefox <jcodefox@gmail.com>
2025-07-30 21:38:05 -0400

    Make ducking affect movement

--- scripts/prefabs/player_v2.gd
+++ scripts/prefabs/player_v2.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var ground_lateral_accel: float = 800
 @export var ground_lateral_drag: float = 0.0002
 @export var air_lateral_accel: float = 0
+@export var crouch_lateral_multiplier: float = 0.25
 
 @export_group("Jumping")
 @export var jump_init_y_velocity: float = -160
@@ -33,25 +34,30 @@ func _physics_process(delta: float) -> void:
 		velocity.y += default_gravity * delta
 	
 	var horizontal_input_axis: float = Input.get_axis("move_left", "move_right")
+	var horizontal_accel_multiplier: float = crouch_lateral_multiplier if Input.is_action_pressed("move_down") else 1.0
 	if is_on_floor():
 		time_since_on_floor = 0
 		extra_jumps_left = max_extra_jumps
 		velocity.y = 0
-		velocity.x += ground_lateral_accel * horizontal_input_axis * delta
+		velocity.x += ground_lateral_accel * horizontal_accel_multiplier * horizontal_input_axis * delta
 		velocity.x *= pow(ground_lateral_drag, delta)
 	else:
-		velocity.x += air_lateral_accel * horizontal_input_axis
+		velocity.x += air_lateral_accel * horizontal_accel_multiplier * horizontal_input_axis
 		velocity *= pow(air_drag, delta)
 	
 	if Input.is_action_pressed("move_down"):
-		set_animation("duckhide")
-	elif velocity.y < 0:
-		set_animation("jump")
-	else:
 		if horizontal_input_axis != 0:
-			set_animation("run_right" if horizontal_input_axis > 0 else "run_left")
+			set_animation("duckwalk")
 		else:
-			set_animation("idle")
+			set_animation("duckhide")
+	else:
+		if velocity.y < 0:
+			set_animation("jump")
+		else:
+			if horizontal_input_axis != 0:
+				set_animation("run_right" if horizontal_input_axis > 0 else "run_left")
+			else:
+				set_animation("idle")
 	
 	
 	if (time_since_on_floor < coyote_time) and (time_since_jump_attempt < jump_buffer_time): # ground jump
@@ -61,10 +67,10 @@ func _physics_process(delta: float) -> void:
 		var direction: float = horizontal_input_axis
 		if direction < 0:
 			if not velocity.x < -1 * air_jump_init_x_velocity:
-				velocity.x = direction * air_jump_init_x_velocity
+				velocity.x = direction * air_jump_init_x_velocity * horizontal_accel_multiplier
 		elif direction > 0:
 			if not velocity.x > air_jump_init_x_velocity:
-				velocity.x = direction * air_jump_init_x_velocity
+				velocity.x = direction * air_jump_init_x_velocity * horizontal_accel_multiplier
 		extra_jumps_left -= 1
 		velocity.y = air_jump_init_y_velocity
 		time_since_jump_attempt = INF
