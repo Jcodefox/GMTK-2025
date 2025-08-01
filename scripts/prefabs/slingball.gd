91cb3a89e9c1291e8c329ab22f39cf0a2682b8e8
Jcodefox <jcodefox@gmail.com>
2025-08-01 11:50:40 -0400

    Give slingball limimted bounces and/or time

--- scripts/prefabs/slingball.gd
+++ scripts/prefabs/slingball.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
+@export var max_bounces: int = 5
+@export var max_lifetime: float = -1
+
 var sprite_ghosts: Array[Node2D] = []
 var collision_shape_ghosts: Array[Node2D] = []
 var area_shape_ghosts: Array[Node2D] = []
 
 var intended_velocity: Vector2 = Vector2.ZERO
+
+var bounces: int = 0
+var cumulative_delta: float = 0
 
 func _ready() -> void:
 	sprite_ghosts = Globals.make_loop_ghosts_of($SuspiciousPlaceholderSlingball)
@@ -14,12 +20,20 @@ func _ready() -> void:
 	$Area2D.body_entered.connect(hit_object)
 
 func _physics_process(delta: float) -> void:
+	cumulative_delta += delta
 	global_position = Globals.apply_loop_teleport(global_position)
 
 	if is_on_wall():
 		intended_velocity.x = -intended_velocity.x
+		bounces += 1
 	if is_on_ceiling() or is_on_floor():
 		intended_velocity.y = -intended_velocity.y
+		bounces += 1
+	
+	if bounces > max_bounces and max_bounces > -1:
+		queue_free()
+	if cumulative_delta > max_lifetime and max_lifetime > -1:
+		queue_free()
 	
 	velocity = intended_velocity
 
