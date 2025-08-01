5b6e1603352525af3082dc7ee309c314f8c31d1a
Jcodefox <jcodefox@gmail.com>
2025-08-01 19:56:55 -0400

    Mothick every other frame

--- scripts/prefabs/enemy/mothick.gd
+++ scripts/prefabs/enemy/mothick.gd
@@ -6,6 +6,7 @@ var animated_sprite_ghosts: Array[Node2D] = []
 var collision_shape_ghosts: Array[Node2D] = []
 var jump_check_shape_ghosts: Array[Node2D] = []
 var time_alive: float = 0
+var frames_alive: int = 0
 
 func _ready() -> void:
 	animated_sprite_ghosts = Globals.make_loop_ghosts_of($AnimatedSprite2D)
@@ -16,7 +17,8 @@ func _process(delta: float) -> void:
 	if time_alive > time_until_enemy_hurts:
 		visible = true
 	else:
-		visible = not visible
+		visible = frames_alive % 4 < 2
+		frames_alive += 1
 
 func _physics_process(delta: float) -> void:
 	time_alive += delta
