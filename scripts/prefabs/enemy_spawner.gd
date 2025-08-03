b877c267c106a6d7c6dcbc5d7696c04561684a4a
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-08-03 00:53:30 -0400

    Add beginner enemy output modes

--- scripts/prefabs/enemy_spawner.gd
+++ scripts/prefabs/enemy_spawner.gd
@@ -12,12 +12,14 @@ enum ENEMY {
 	EXPRESS_BOT = 6,
 }
 
-var time_until_new_out_mode: float = 0.0
+var time_until_new_out_mode: float = 2.0
 var out_mode: int = OUTMODES.PAUSE
 enum OUTMODES {
 	PAUSE,
 	
+	SPIKROS,
 	MIXED_SPIKROS_MOTHS,
+	MOTHICKS,
 	MOTH_BOMB,
 	
 	FULL_RANDOM,
@@ -25,35 +27,80 @@ enum OUTMODES {
 
 var spawn_rate_min_wait: float = 1.0
 var spawn_rate_max_wait: float = 2.0
-var time_to_next_spawn: float = 1.5
+var time_to_next_spawn: float = 0.0
 
-func change_out_mode_to(out_mode: int):
+func spawn_enemy(enemy_type: int, direction: int = 0) -> void:
+	var new_enemy: Node2D = enemy_scenes[enemy_type].instantiate()
+	new_enemy.global_position = global_position
+	new_enemy.position.x += (20 * [-1, 1].pick_random()) if direction == 0 else (20 * direction)
+	new_enemy.z_index = 32
+	enemy_holder_node.add_child(new_enemy)
+
+func change_out_mode_to(out_mode_input: int) -> void:
+	out_mode = out_mode_input
+	time_to_next_spawn = 0
+	time_until_new_out_mode = randf_range(1.0, 4.0)
+	spawn_rate_min_wait = 0.5; spawn_rate_max_wait = 1.0
 	match out_mode:
 		OUTMODES.PAUSE:
-			time_until_new_out_mode = randf_range(1.0, 5.0)
-	pass
+			time_until_new_out_mode = randf_range(1.0, 4.0)
+			spawn_rate_min_wait = 0.0; spawn_rate_max_wait = 1.0
+		OUTMODES.SPIKROS, OUTMODES.MIXED_SPIKROS_MOTHS, OUTMODES.MOTHICKS:
+			time_until_new_out_mode = randf_range(4.0, 6.0)
+			spawn_rate_min_wait = 1.5; spawn_rate_max_wait = 2.5
+		OUTMODES.MOTH_BOMB:
+			time_until_new_out_mode = randf_range(0.5, 0.75)
+			spawn_rate_min_wait = 0.1; spawn_rate_max_wait = 0.2
 
 func _process(delta: float) -> void:
 	time_until_new_out_mode -= delta
-	if time_until_new_out_mode <= 0.0:
-		if (Globals.time_passed > 0) and (Globals.time_passed < 30):
-			match randi_range(0,2):
-				0:
-					pass
-				1:
-					pass
-				2:
-					pass
-		else:
-			change_out_mode_to(OUTMODES.PAUSE)
-
-func _physics_process(delta: float) -> void:
 	time_to_next_spawn -= delta
 	
-	if time_to_next_spawn <= 0.0:
-		var new_enemy: Node2D = enemy_scenes.pick_random().instantiate()
-		new_enemy.global_position = global_position 
-		var output_direction = (randi_range(0, 1) * 2) - 1 # random +/- 1
-		new_enemy.position.x += 200 * output_direction
-		enemy_holder_node.add_child(new_enemy)
-		time_to_next_spawn = randf_range(1.0, 7.0)
+	if time_until_new_out_mode <= 0.0:
+		time_to_next_spawn = 0
+		if (Globals.time_passed > 0) and (Globals.time_passed < 999):
+			match randi_range(0,5):
+				0, 1:
+					change_out_mode_to(OUTMODES.PAUSE)
+				2:
+					change_out_mode_to(OUTMODES.SPIKROS)
+				3, 4:
+					change_out_mode_to(OUTMODES.MIXED_SPIKROS_MOTHS)
+				5:
+					change_out_mode_to(OUTMODES.MOTHICKS)
+				6:
+					change_out_mode_to(OUTMODES.MOTH_BOMB)
+		else:
+			change_out_mode_to(OUTMODES.FULL_RANDOM)
+	
+	if time_to_next_spawn < 0:
+		time_to_next_spawn = randf_range(spawn_rate_min_wait, spawn_rate_max_wait)
+		match out_mode:
+			OUTMODES.PAUSE:
+				pass
+			OUTMODES.SPIKROS:
+				spawn_enemy(ENEMY.SPIKRO)
+			OUTMODES.MIXED_SPIKROS_MOTHS:
+				match randi_range(0, 1):
+					0:
+						spawn_enemy(ENEMY.SPIKRO)
+					1:
+							spawn_enemy(ENEMY.MOTHICK)
+			OUTMODES.MOTH_BOMB:
+				spawn_enemy(ENEMY.MOTHICK)
+			OUTMODES.FULL_RANDOM:
+				match randi_range(0, 6):
+					0:
+						spawn_enemy(ENEMY.SPIKRO)
+					1:
+						spawn_enemy(ENEMY.MOTHICK)
+					2:
+						spawn_enemy(ENEMY.SHOCK_WISP)
+					3:
+						spawn_enemy(ENEMY.SPROING)
+					4:
+						spawn_enemy(ENEMY.NAMELESS)
+					5:
+						spawn_enemy(ENEMY.DRONE)
+					6:
+						spawn_enemy(ENEMY.EXPRESS_BOT)
