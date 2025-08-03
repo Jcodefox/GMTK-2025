e19f8b7d6e51a64032bed424d6606546715600a2
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-08-02 23:46:44 -0400

    Unfinished revised enemy spawning mechanics

--- scripts/prefabs/enemy_spawner.gd
+++ scripts/prefabs/enemy_spawner.gd
@@ -2,16 +2,58 @@ extends Node2D
 
 @export var enemy_holder_node: Node2D
 @export var enemy_scenes: Array[PackedScene] = []
-var time_to_next: float = 2.0
+enum ENEMY {
+	SPIKRO = 0,
+	MOTHICK = 1,
+	SHOCK_WISP = 2,
+	SPROING = 3,
+	NAMELESS = 4,
+	DRONE = 5,
+	EXPRESS_BOT = 6,
+}
 
-func _ready() -> void:
-	time_to_next = randf_range(1.0, 2.0)
+var time_until_new_out_mode: float = 0.0
+var out_mode: int = OUTMODES.PAUSE
+enum OUTMODES {
+	PAUSE,
+	
+	MIXED_SPIKROS_MOTHS,
+	MOTH_BOMB,
+	
+	FULL_RANDOM,
+}
+
+var spawn_rate_min_wait: float = 1.0
+var spawn_rate_max_wait: float = 2.0
+var time_to_next_spawn: float = 1.5
+
+func change_out_mode_to(out_mode: int):
+	match out_mode:
+		OUTMODES.PAUSE:
+			time_until_new_out_mode = randf_range(1.0, 5.0)
+	pass
+
+func _process(delta: float) -> void:
+	time_until_new_out_mode -= delta
+	if time_until_new_out_mode <= 0.0:
+		if (Globals.time_passed > 0) and (Globals.time_passed < 30):
+			match randi_range(0,2):
+				0:
+					pass
+				1:
+					pass
+				2:
+					pass
+		else:
+			change_out_mode_to(OUTMODES.PAUSE)
 
 func _physics_process(delta: float) -> void:
-	time_to_next -= delta
-
-	if time_to_next <= 0.0:
+	time_to_next_spawn -= delta
+	
+	if time_to_next_spawn <= 0.0:
 		var new_enemy: Node2D = enemy_scenes.pick_random().instantiate()
-		new_enemy.global_position = global_position
+		new_enemy.global_position = global_position 
+		var output_direction = (randi_range(0, 1) * 2) - 1 # random +/- 1
+		new_enemy.position.x += 200 * output_direction
 		enemy_holder_node.add_child(new_enemy)
-		time_to_next = randf_range(1.0, 7.0)
+		time_to_next_spawn = randf_range(1.0, 7.0)
